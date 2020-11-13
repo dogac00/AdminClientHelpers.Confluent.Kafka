a1738d31952d7754cdba30cb7c5e391883557c0c
Dogac <ndakyildiz@gmail.com>
2020-11-14 02:22:42 +0300

    Add CreateDefaultTopicAsync.

--- src/AdminClientExtensions.cs
+++ src/AdminClientExtensions.cs
@@ -1,6 +1,21 @@
-﻿namespace AdminClient.Extensions
+﻿using System.Threading.Tasks;
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+
+namespace AdminClient.Extensions
 {
     public static class AdminClientExtensions
     {
+        public static async Task CreateDefaultTopicAsync(this IAdminClient client, string topic)
+        {
+            await client.CreateTopicsAsync(new TopicSpecification[]
+            {
+                new TopicSpecification
+                {
+                    Name = topic,
+                    NumPartitions = 10
+                }
+            });
+        }
     }
 }
